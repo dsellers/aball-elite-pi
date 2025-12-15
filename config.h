11191f4cfba405c341a5eb6fff33ed4070904e69
dsellers <daniel.n.sellers@gmail.com>
2025-12-14 20:37:57 -0500

    adding console and debug commands for testing

--- config.h
+++ config.h
@@ -17,6 +17,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define CONSOLE_ENABLED                     // As soon as the sensor detects movement, the MOT pin is pulled low (active)
+#define POINTING_DEVICE_DEBUG               // QMK logs this automatically if debug is enabled
+
 // ADNS-9800 wiring
 #define ADNS9800_CS_PIN             A0      // SEL / NCS on A0
 #define POINTING_DEVICE_MOTION_PIN  G27     // MOT pin: A1 on Elite-Pi = GP27 on RP2040
